760cfc8a6f37ecd02c118e700e4760ae8817b498
Michael V <bolsunovskiy@gmail.com>
2015-09-10 23:54:41 +0300

    implement auth

--- www/js/src/modules/settings/controllers/index.js
+++ www/js/src/modules/settings/controllers/index.js
@@ -1,21 +1,31 @@
-angular.module('settings').controller('backendSettings',  ['$scope', 'settings.auth', function($scope, auth){
+angular.module('settings').controller('backendSettings',  ['$scope', 'settings.auth', 'storage', '$location',
+    function($scope, auth, storage, $location){
+
+    if(storage.isSet('backend') && storage.isSet('user')) {
+        $location.path('/beacons');
+    } else {
+        $scope.backend = {
+            url: 'http://'
+        };
+    }
 
 
-    $scope.backend = {
-        url: 'http://'
-    };
 
     $scope.form = {
+        error: false,
         submitted: false,
         submit: function(){
 
             $scope.form.submitted = true;
 
             if($scope.frm.$valid) {
-                auth.login($scope.backend.url, $scope.backend.email, $scope.backend.password, function(e){
-                    console.log(e);
-                }, function(e){
-                    console.log(e);
+                auth.login($scope.backend.url, $scope.backend.email, $scope.backend.password, function(user){
+                    storage.set('backend', $scope.backend);
+                    storage.set('user', user);
+
+                    $location.path('/beacons');
+                }, function(){
+                    $scope.form.error = true;
                 });
             }
         }
--- www/js/src/modules/settings/services/auth.js
+++ www/js/src/modules/settings/services/auth.js
@@ -2,6 +2,7 @@ angular.module('settings').factory('settings.auth', ['$http', function($http){
 
     return {
         login: function(url, email, password, success, error) {
+
             $http.post(url + '/v1/users/login', {
                 email: email,
                 password: password
